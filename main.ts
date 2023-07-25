17e05964f7250a06a13d198ba550874b6028d269
Benjamin Audren <benjamin.audren@ik.me>
2023-07-25 21:11:49 +0200

    Temp first version

--- main.ts
+++ main.ts
@@ -1,81 +1,25 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, MarkdownPostProcessorContext, MarkdownRenderChild, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { parseSong, renderSong } from 'chord-mark/lib/chord-mark.js';
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface ChordMarkPluginSettings {
+	theme: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: ChordMarkPluginSettings = {
+	theme: 'cmTheme-dark1'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ChordMarkPlugin extends Plugin {
+	settings: ChordMarkPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		this.registerMarkdownCodeBlockProcessor("chord-mark", this.codeProcessor); 
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new ChordMarkSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -89,28 +33,40 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	async codeProcessor (source: string, el: HTMLElement, ctx: MarkdownPostProcessorContext) {
+		ctx.addChild(new ChordMarkRender(source, el, "cmTheme-dark1"));
+		
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class ChordMarkRender extends MarkdownRenderChild {
+  
+	constructor(
+		private readonly source: string,
+		private readonly el: HTMLElement,
+		private readonly theme: string,
+	) {
+		super(el); // important
+	}
+  
+	onload() {
+		const parsed = parseSong(this.source);
+		const rendered = renderSong(parsed);
+		let div = document.createElement('div');
+		div.addClass(this.theme);
+		div.setAttribute("font-family", "monospace");
+		div.innerHTML = rendered;
+		this.el.appendChild(div);
+	}
 
-	constructor(app: App, plugin: MyPlugin) {
+}
+  
+
+class ChordMarkSettingTab extends PluginSettingTab {
+	plugin: ChordMarkPlugin;
+
+	constructor(app: App, plugin: ChordMarkPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -124,13 +80,12 @@ class SampleSettingTab extends PluginSettingTab {
 
 		new Setting(containerEl)
 			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setDesc('Chord-Mark theme to use')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('cmTheme-dark1')
+				.setValue(this.plugin.settings.theme)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.theme = value;
 					await this.plugin.saveSettings();
 				}));
 	}
